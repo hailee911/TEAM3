3d539d1b083a4b9b70d4f42ce30271b118e2000c
hyundingi <qo0723@naver.com>
2024-12-13 15:16:24 +0900

    121313218

--- w01/calendar1/admin.py
+++ w01/calendar1/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from calendar1.models import Event
 
 @admin.register(Event)
-class MemberAdmin(admin.ModelAdmin):
-    list_display = ('no', 'name', 'title', 'location', 'repeat')
-    search_fields = ('no', 'name', 'title')  # 검색 필드 추가
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['no', 'name', 'title', 'location', 'repeat']
+    search_fields = ['no', 'name', 'title']  # 검색 필드 추가
