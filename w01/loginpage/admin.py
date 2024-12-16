0bbe79d2686a88929d31e4e229d30dbdbd62c54c
yjkim333 <kyjoon0105@gmail.com>
2024-12-16 19:59:53 +0900

    Delete w01 directory

--- w01/loginpage/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Member
-
-@admin.register(Member)
-class MemberAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'mail', 'gender', 'mdate','created_group','joined_group')
-    search_fields = ('id', 'name', 'mail')  # 검색 필드 추가
-    
-    
